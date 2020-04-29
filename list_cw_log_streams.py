ec41d9f55996c124474bdba93761aaff013761d6
jjones3000 <jeff@gridlevelwd.com>
2020-04-29 13:21:08 -0500

    List cloudwatch logstreams

---
+++ list_cw_log_streams.py
@@ -0,0 +1,20 @@
+import os
+import json
+import boto3
+
+client = boto3.client('logs')
+paginator = client.get_paginator('describe_log_groups')
+iterator = paginator.paginate(PaginationConfig={'MaxItems': 1000})
+for page in iterator:
+    #print("*** Page")
+    for i in page['logGroups']:
+        #print(i['logGroupName'])
+        print(i)
+#lss = client.describe_log_streams()
+#for ls in lss:
+  #print(ls)
+
+
+cw_client = boto3.client('cloudwatch')
+lgs = cw_client.describe_log_groups()
+print(lgs
